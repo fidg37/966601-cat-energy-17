383ca28b99afc4de0bd1ed851f535c4588c3d845
Nikita <fidg37@gmail.com>
2019-09-01 15:38:25 +0300

    Исправил замечания по слайдеру и подогнал под pixel perfect

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,10 +3,10 @@ var button = menu.querySelector(".main-navigation__toggle");
 
 var slider = document.querySelector(".slider");
 var range = slider.querySelector(".range");
-var slideFat = slider.querySelector(".slider__item--fat-cat");
-var slideSlim = slider.querySelector(".slider__item--slim-cat");
+var slideBefore = slider.querySelector(".slider__item--before");
+var slideAfter = slider.querySelector(".slider__item--after");
 var screenWidth = document.documentElement.clientWidth;
-var slideWidth = slide.offsetWidth;
+var sliderWrapper = document.querySelector(".example__container-slider")
 
 menu.classList.remove("main-navigation--no-js");
 
@@ -21,39 +21,12 @@ if (screenWidth < 768) {
   range.value = 50;
 };
 
-range.addEventListener("click", function() {
-  var rangeValue = range.value;
-  if (screenWidth < 768) {
-    if (rangeValue < 50) {
-      range.value = 0;
-      slideFat.style.width = 100 + "%";
-      slideSlim.style.width = 0 + "%";
-    } else {
-      range.value = 100;
-      slideFat.style.width = 0 + "%";
-      slideSlim.style.width = 100 + "%";
-    };
-  } else if (rangeValue < 40) {
-      range.value = 0;
-      slideFat.style.width = 100 + "%";
-      slideSlim.style.width = 0 + "%";
-    } else if (rangeValue > 60) {
-      range.value = 100;
-      slideFat.style.width = 0 + "%";
-      slideSlim.style.width = 100 + "%";
-    } else {
-      range.value = 50;
-      slideFat.style.width = 50 + "%";
-      slideSlim.style.width = 50 + "%";
-  };
-});
-
-var slider = document.querySelector(".slider");
-var range = slider.querySelector(".range");
-var slideFat = slider.querySelector(".slider__item--fat-cat");
-var slideSlim = slider.querySelector(".slider__item--slim-cat");
-
 range.addEventListener("input", function () {
-  slideFat.style.width = 100 - Math.floor(range.value) + "%";
-  slideSlim.style.width = Math.floor(range.value) + "%";
+  var screenWidth = document.documentElement.clientWidth;
+
+  slideBefore.style.width = 100 - Math.floor(range.value) + "%";
+
+  if (screenWidth >= 1300) {
+    sliderWrapper.style.background ='linear-gradient(to right, #f2f2f2 ' + (100 - Math.floor(range.value)) + '%, #eaeaea ' + (100 - Math.floor(range.value)) + '%';
+  };
 });
